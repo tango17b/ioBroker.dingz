c0246e4d3659d0e10ee9236929de0e16631c9ad6
tango17b <75530922+tango17b@users.noreply.github.com>
2020-12-24 16:22:45 +0100

    Update main.js

--- build/main.js
+++ build/main.js
@@ -150,9 +150,10 @@ class Dingz extends utils.Adapter {
             else {
                 // change came from the device. 
 				//update all values
-				
-				this.fetchValues();
-				this.log.silly('fetch values due to device action');
+				if (subid.startsWith('actions')){
+					this.fetchValues();
+					this.log.silly('fetch values due to button action');
+				}
 				
 				//If it was the PIR, track it until no more motion is detected
                 if (id.endsWith("pir.generic")) {
