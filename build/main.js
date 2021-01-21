02f0ceb423339e83c16425ca8a7ae348bb2fe312
tango17b <75530922+tango17b@users.noreply.github.com>
2021-01-21 22:49:56 +0100

    Update main.js

--- build/main.js
+++ build/main.js
@@ -162,7 +162,7 @@ class Dingz extends utils.Adapter {
 				this.setStateAsync(`shades.${n}.blind`, blind.position, true);
 				this.setStateAsync(`shades.${n}.lamella`, blind.lamella, true);
 				
-				if (this.getState(`shades.${n}.up`).val() || this.getState(`shades.${n}.down`).val() || this.getState(`shades.${n}.stop`).val()) {
+				if (this.getState(`shades.${n}.up`).val || this.getState(`shades.${n}.down`).val || this.getState(`shades.${n}.stop`).val) {
 					this.setStateAsync(`shades.${n}.up`, false, true);
 					this.setStateAsync(`shades.${n}.down`, false, true);
 					this.setStateAsync(`shades.${n}.stop`, false, true);						
